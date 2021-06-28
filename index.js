d56a42297fb21d0c0330d77eb3c14f87d32ecf95
Karla Huerta <KMHS@MacBook-Air-de-Karla.local>
2021-06-28 12:06:01 -0500

    actualizacion

--- index.js
+++ index.js
@@ -6,17 +6,28 @@ const valorA        = document.getElementById("valor-a")
 const valorB        = document.getElementById("valor-b")
 const btnSumar      = document.getElementById("btn-sumar")
 const areaSuma      = document.getElementById("area-suma")
+const valorC        = document.getElementById("valor-c")
+const valorD        = document.getElementById("valor-d")
+const btnRestar     = document.getElementById("btn-restar")
+const areaRestar    = document.getElementById("area-restar")
 
 // 2. FUNCIONES
 const saludar = (name) => {
     const saludo = `Hola ${name}!!!`
     return areaSaludo.innerHTML = saludo
 }
+/**
+ * DENTRO DE LA PROGRAMACIÓN FUNCIONAL, EXISTE UN CONCEPTO LLAMADO
+ * PUREZA.
+ * LA PUREZA DE LAS FUNCIONES TE DICE QUE DEBEN SER LO MÁS INDEPENDIENTES POSIBLES. SUS ÚNICOS ACCESOS ESTÁN DENTRO DE LA MISMA FUNCIÓN Y A TRAVÉS DE SUS PARÁMETROS. NO MÁS.
+ * */ 
 const sumar = (a, b) => {
-    const resultado = a + b
-    return areaSuma.innerHTML = resultado
+    return a + b
 }
 
+const restar = (c, d) => {
+    return c - d
+}
 // 3. EVENTOS
 btnSaludar.addEventListener("click", () => {
     const nameValue = nombre.value
@@ -25,5 +36,15 @@ btnSaludar.addEventListener("click", () => {
 btnSumar.addEventListener("click", () => {
     const inputValorA   = parseInt(valorA.value)
     const inputValorB   = parseInt(valorB.value)
-    return sumar(inputValorA, inputValorB)
+    const resultado = sumar(inputValorA, inputValorB)
+    areaSuma.innerHTML = resultado
+})
+btnRestar.addEventListener("click",() => {
+    const inputValorC   = parseInt(valorC.value)
+    const inputValorD   = parseInt(valorD.value)
+    const resultado = restar(inputValorC, inputValorD)
+    console.log (inputValorC)
+    console.log (inputValorD)
+    console.log (resultado)
+    areaRestar.innerHTML = resultado 
 })
